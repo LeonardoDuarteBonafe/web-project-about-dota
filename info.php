691f62769acee02ef4ba31ff6053c25e0f8bcc12
LeonardoDuarteBonafe <32874289+LeonardoDuarteBonafe@users.noreply.github.com>
2017-10-31 01:05:55 -0200

    Add files via upload

---
+++ info.php
@@ -0,0 +1,110 @@
+<?php include 'header.php';
+$pHeader = new header();
+?>
+<?php include 'footer.php';
+$pFooter = new footer();
+?>
+<html>
+
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, inicial-scale=1">
+        <title>Curiosidades, fatos e dicas!</title>
+        <link rel="stylesheet" type="text/css" href="style.css"> 
+        <link rel="stylesheet" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+        <link rel="icon" href="dota2iconTitle.png">
+    </head>
+
+    <body>
+    <?php $pHeader->printHeader()?>
+        <div id="info_background">
+            <h1 class="title info_title">Curiosidades, fatos e dicas aqui!</h1>
+            <p class="info_p">Nesta seção poderemos ver o básico sobre dota 2, sobre seu principal campeonato e algumas jogadas que consagraram os times como melhores do mundo!</p>
+        </div>
+        <div class="info_carousel">
+                <img src="img/info/imgGrandes/dotati1rend.png" class="info_img">
+                <img src="img/info/imgGrandes/dotati2rend.png" class="info_img">
+                <img src="img/info/imgGrandes/dotati3rend.png" class="info_img">
+                <img src="img/info/imgGrandes/dotati4rend.png" class="info_img">
+                <img src="img/info/imgGrandes/dotati5rend.png" class="info_img">
+                <img src="img/info/imgGrandes/dotati6rend.png" class="info_img">
+                <img src="img/info/imgGrandes/dotati7rend.png" class="info_img">
+        </div>
+        <div>
+            <p class="info_p">Dota 2 teve seu começo em 2011, dessa forma veio o primeiro The Internacional. E houve as suas sequências com os prêmios multimilionários. Vejamos abaixo algumas informações</p>
+            <div>
+                <article  class="article info_article">
+                    <p>The internacional 1 - Aconteceu no ano de 2011, contou com 16 equipes. Tendo como primeira campeã a equipe ucraniana Natus Vincere (NA'VI), com um prêmio total de $1,000,000. 
+                    </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/1VkZm0cQ0LM" frameborder="0" allowfullscreen></iframe>
+                </article>
+                
+                <article class="article info_article">
+                    <p>The internacional 2 - Aconteceu no ano de 2012, contou com 16 equipes. Tendo uma final disputada entre Natus Vincere (NA'VI), primeira campeã, e Invictus Gaming (IG). A equipe chinesa IG levou a melhor e faturou cerca de $1,000,000.
+                    </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/fM3z7C2lV8U" frameborder="0" allowfullscreen></iframe>
+                    
+                </article>
+                
+                <article class="article info_article">
+                    <p>The internacional 3 - Aconteceu no ano de 2013, contou com 16 equipes. Sua final aconteceu entre Alliance e Natus Vincere, finalista pela terceira vez. Foi uma final muito emocionante, aconterem 5 partidas extremamentes incriveis e no final quem levou o prêmio de $1,437,190 para a casa foi a equipe européia Alliance em uma vitória por 3x2.
+                    </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/krHujKemMXs" frameborder="0" allowfullscreen></iframe>
+                </article>
+                
+                <article class="article info_article">
+                    <p>The internacional 4 - Aconteceu no ano de 2014, contou com 16 equipes. Nessa temporada o time da NA'VI fora eliminado rapidamente. E o campeão foi a equipe chinesa Newbee, com uma premiação $5,025,029. Foi uma das finais mais rápidas, com jogos de apenas 15 minutos. Porém a Newbee conseguiu levar a melhor contra o time da Vici Gaming (VG).
+                    </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/9Qu-Pk-LgpI" frameborder="0" allowfullscreen></iframe>
+                </article>
+                
+                <article class="article info_article">
+                    <p>The internacional 5 - Aconteceu no ano de 2015, contou com 16 equipes. Esse foi um ano um pouco diferente, com muitas surpresas e reviravoltas, mas no final quem levou o prêmio foi a equipe americana, Evil Genisus (EG). Levando para casa $6,634,661.
+                    </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/ERTIWNfx93w" frameborder="0" allowfullscreen></iframe>
+                </article>
+                
+                <article class="article info_article">
+                    <p>The internacional 6 - Aconteceu no ano de 2016, contou com 16 equipes. E a campeã foi a equipe chinesa, Wings, ganhando $9,139,002. Esse foi um ano de surpresas, a final foi Wings x Digital Chaos, dois times em que o público tinha baixas espectativas, mas que superaram tudo e fizeram por merecer
+                    </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/4Us8X5PGt58" frameborder="0" allowfullscreen></iframe>
+                </article>
+                
+                <article class="article info_article">
+                    <p>The internacional 7 - Aconteceu no ano de 2017, contou com 18 equipes, isso foi uma novidade. O time da Liquid lutou bravamente após quase serem eliminados. Com garra e determinação eles fizeram história, ganhnaod uma final por 3x0, resultado inédito. A final aconteceu entre Team Liquid x Team Newbee. A Liquid levou $10,862,683.
+                    </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/Sulu9myTVUg" frameborder="0" allowfullscreen></iframe>
+                </article>
+                
+                <article class="article info_article">
+                    <p>Aqui vamos ter um compilado dos melhores momentos, envolvendo team play, solo play, muitas jogadas realmente lindas, narrações espetaculares, vale a pena conferir. Consta com jogadas do: </p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/rqFVMj8wk3M" frameborder="0" allowfullscreen></iframe>
+                    <ul>
+                        <li>The international 1</li>
+                        <li>The international 2</li>
+                        <li>The international 3</li>
+                        <li>The international 4</li>
+                        <li>The international 5</li>
+                    </ul>     
+                </article>
+                
+                <article class="article info_article">
+                    <p>Aqui temos uns momentos que tocam meu coração. I am a NA'VI Fanboy, so i like this video so much!</p>
+                    <iframe width="560" height="315" src="https://www.youtube.com/embed/idEUCU6SDoU" frameborder="0" allowfullscreen></iframe>
+                </article>
+                
+                <article class="article  info_article info_dicas">
+                    <a href="http://www.dota2.com.br/herois/" target="_blank"><img src="img/info/dotaIniciantes.jpg"></a>
+                    <a href="http://wiki.teamliquid.net/dota2/Main_Page" target="_blank"><img src="img/info/dotaIniciantes2.jpg"></a>
+                    <a href="http://www.invokergame.com/" target="_blank"><img src="img/info/invokerGame.jpg"></a>
+                </article>
+                
+            </div>
+        </div>
+        
+        <?php $pFooter->printFooter()?>
+        <script type="text/javascript" src="script.js"></script>
+    </body>
+
+
+</html>
---
+++ script.js
@@ -0,0 +1,48 @@
+/* NAVBAR FIXED E TRANSLUCIDA */
+var header;
+window.addEventListener("scroll", yScroll);        
+function yScroll(){
+    header= document.getElementById("header");
+    yPos = window.pageYOffset;
+    if(yPos>header.offsetHeight){
+        
+        header.style.position = "fixed";
+        header.style.background = "linear-gradient(to right, rgba(193,33,17, 0.7) , rgba(58,13,8, 0.7)";
+    }   
+    if(yPos<header.offsetHeight){
+        header.style.position = "static";
+        header.style.background = "linear-gradient(to left, #C12111 , #3A0D08)";
+    }
+}
+
+document.getElementById("btn_menu_id").onclick = function(){
+    document.getElementById("menu_id").style.display = "block";
+}
+document.getElementById("btn_close_id").onclick = function(){
+    document.getElementById("menu_id").style.display = "none";
+}
+
+document.getElementById("banner_download").onclick = function() {
+    location.href="download.php";
+}
+
+document.getElementById("form_enviar").onclick = function(){
+    alert("Agradecemos o seu envio, irá nos ajudar a melhorar, volte sempre que desejar!");
+    location.href="index.php";
+}
+
+document.getElementById("form_cancelar").onclick = function(){
+    location.href="index.php";
+}
+document.getElementById("form_button_btn1").onclick = function(){
+ alert("Nós te amamos e queremos você conosco todos os dias!");
+} 
+document.getElementById("form_button_btn2").onclick = function(){
+    var randomNumber = Math.floor((Math.random()*6)+1);
+    var userNumber = document.getElementById("user_number").value;
+    if(randomNumber == userNumber){
+        alert("Parabéns, você acertou");
+    } else {
+        alert("Você errou, mas tente de novo, o número era " + randomNumber);
+    }
+}
